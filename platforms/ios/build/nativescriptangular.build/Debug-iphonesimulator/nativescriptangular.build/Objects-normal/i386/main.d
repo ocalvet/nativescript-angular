0ce1e38e5e761b6102fc4bc0e903d4586922e765
Ovidio R. Calvet <calveto@gmail.com>
2016-05-05 21:32:49 -0400

    Minimun nativescript app

---
+++ platforms/ios/build/nativescriptangular.build/Debug-iphonesimulator/nativescriptangular.build/Objects-normal/i386/main.d
@@ -0,0 +1,8 @@
+dependencies: \
+  /Users/ovidio/Projects/testProjects/nativescript-angular/platforms/ios/internal/main.m \
+  /Users/ovidio/Projects/testProjects/nativescript-angular/platforms/ios/internal/NativeScript/include/NativeScript.h \
+  /Users/ovidio/Projects/testProjects/nativescript-angular/platforms/ios/internal/NativeScript/include/TNSRuntime.h \
+  /Users/ovidio/Projects/testProjects/nativescript-angular/platforms/ios/internal/NativeScript/include/TNSRuntime+Diagnostics.h \
+  /Users/ovidio/Projects/testProjects/nativescript-angular/platforms/ios/internal/NativeScript/include/TNSRuntime+Inspector.h \
+  /Users/ovidio/Projects/testProjects/nativescript-angular/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/ovidio/Projects/testProjects/nativescript-angular/platforms/ios/internal/TNSDebugging.h
